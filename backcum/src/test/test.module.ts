2c009bdab7e58ab79445055e466863f8d0629d72
Omarius016 <omarbeltran210@gmail.com>
2025-05-08 02:04:35 -0700

    ASEREJE

---
+++ backcum/src/test/test.module.ts
@@ -0,0 +1,10 @@
+// src/test/test.module.ts
+import { Module } from '@nestjs/common';
+import { TestService } from './test.service';
+import { TestController } from './test.controller';
+
+@Module({
+  controllers: [TestController],
+  providers: [TestService],
+})
+export class TestModule {}
