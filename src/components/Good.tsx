ff1d139898ef6a88450dce3e58c5e1a3bafcbcbd
Omarius016 <omarbeltran210@gmail.com>
2025-06-05 19:50:04 -0700

    Justice

--- src/components/Good.tsx
+++ src/components/Good.tsx
@@ -1,18 +1,39 @@
+import "./GoodFireworks.css";
+
 const Good = () => {
   return (
-    <div style={{
-      display: "flex",
-      flexDirection: "column",
-      alignItems: "center",
-      justifyContent: "center",
-      minHeight: "80vh"
-    }}>
-      
+    <div
+      style={{
+        position: "relative",
+        width: "100vw",
+        height: "100vh",
+        background: "linear-gradient(#0007, #0000), #123",
+        overflow: "hidden",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center"
+      }}
+    >
+      {/* Imagen al fondo */}
       <img
-        src="/good.jpg" 
+        src="/good.jpg"
         alt="Good"
-        style={{ maxWidth: "90vw", maxHeight: "60vh", marginTop: 24 }}
+        style={{
+          maxWidth: "90vw",
+          maxHeight: "60vh",
+          marginTop: 24,
+          borderRadius: "32px",
+          boxShadow: "0 8px 32px #000a",
+          zIndex: 0,
+          position: "absolute", // <-- importante
+        }}
       />
+      {/* Fuegos artificiales encima */}
+      <div className="firework"></div>
+      <div className="firework"></div>
+      <div className="firework"></div>
+      <div className="firework"></div>
+      <div className="firework"></div>
     </div>
   );
 };
